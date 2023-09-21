5246db76b7c8e70a6ea3c01bce2c6c0d68e6fcd8
balint-szabox <70958880+balint-szabox@users.noreply.github.com>
2023-09-21 18:45:07 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace SteamGameStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LaunchGame();
+        }
+
+        static void LaunchGame()
+        {
+            string steamPath = @"C:\Program Files (x86)\Steam\Steam.exe";
+            string appID = "2225070";
+
+            try
+            {
+                Process.Start(steamPath, "-applaunch " + appID);
+                Console.WriteLine($"A játék (AppID: {appID}) elindult.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba történt a játék indítása közben: {ex.Message}");
+            }
+        }
+    }
+}
